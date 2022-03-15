1e9a445e69f7388e168510d9820656c74abba55b
gentigashi <genti.gashi.fi@gmail.com>
2022-03-15 20:18:21 +0200

    ex3.js
    Add Ex3

---
+++ Lecture 4/ex3.js
@@ -0,0 +1,55 @@
+
+
+//const add=(a,b) => a + b;
+//console.log(add(2,3))
+
+function add(a,b) {
+    return a+b
+  }
+  console.log(add(2,3))
+
+  
+  //const square(n) => n * n;
+  //console.log(square(3))
+  function square(n) {
+    return n * n
+  }
+  console.log(square(3)) 
+
+
+  /*const myFunc(a,b) => {
+      let chuck =42;
+      return a + b + chuck;
+  }
+  console.log(myFunc(2,3))
+*/
+  function myFunc(a, b){
+    let chuck = 42;
+    return a + b + chuck;
+  }
+  console.log(myFunc(2,3))
+
+
+  //const sayHello = () => "Hello there";
+  //console.log(sayHello());
+
+  function sayHello() {
+    return "Hello there";
+}
+console.log(sayHello());
+
+//const myObj= () =>({cost: 500})
+//console.log(myObj());
+
+function myObj() {
+    return { cost: 500};
+}
+console.log(myObj());
+
+
+
+
+
+
+
+
